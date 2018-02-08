26ebc89e95cb1a997fb182e997ca529be1194406
keff6 <kev.fallas@gmail.com>
2018-02-08 17:02:26 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,146 @@
+import React, {
+  Component
+} from 'react';
+import './App.css';
+
+const largeColumn = { width: '40%'},
+      midColumn = { width: '30%' },
+      smallColumn = { width: '10%'},
+      DEFAULT_QUERY = 'redux',
+      PATH_BASE = 'https://hn.algolia.com/api/v1',
+      PATH_SEARCH = '/search',
+      PARAM_SEARCH = 'query=';
+      //url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;     
+
+/*
+function isSearched(searchTerm) { 
+  return function(item) { 
+    // some condition which returns true or false 
+        return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  } 
+}*/
+
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      result: null,
+      searchTerm: DEFAULT_QUERY,
+    };
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this); 
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this); 
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this); 
+  }
+
+  setSearchTopStories(result) { 
+    this.setState({ result }); 
+  }
+
+  fetchSearchTopStories(searchTerm) { 
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`) 
+      .then(response => response.json()) 
+      .then(result => this.setSearchTopStories(result)) 
+      .catch(e => e); 
+    }
+
+  componentDidMount() { 
+    const { searchTerm } = this.state; 
+    this.fetchSearchTopStories(searchTerm); 
+  }
+
+  onSearchSubmit(event) { 
+    const { searchTerm } = this.state; 
+    this.fetchSearchTopStories(searchTerm); 
+    event.preventDefault();
+  }
+
+  onDismiss(id){
+    const isNotId = item => item.objectID !== id; 
+    const updatedHits = this.state.result.hits.filter(isNotId); 
+    this.setState({ 
+      result: { ...this.state.result, hits: updatedHits } 
+    });
+  }
+
+  onSearchChange(event){
+    this.setState({ searchTerm: event.target.value });
+
+  }
+
+  render() {
+    const { searchTerm, result} = this.state;
+
+    return ( 
+      <div className = "page" >
+        <div className = "interactions" >
+          <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+          </Search>
+        </div>
+        { result &&
+          <Table 
+            list={result.hits}
+            onDismiss={this.onDismiss}
+          /> 
+        }      
+      </div>
+    );
+  }
+}
+
+const Search = ({ value, onChange, onSubmit, children }) =>   
+  <form onSubmit={onSubmit}> 
+    <input 
+      type="text" 
+      value={value}
+      onChange={onChange}
+    /> 
+    <button type="submit">
+      {children} 
+    </button>
+  </form>      
+
+
+const Table = ({ list, onDismiss }) =>
+  <div className="table">
+    {list.map((item) => 
+      <div  key={item.objectID} className="table-row"> 
+        <span style={largeColumn}> 
+          <a href={item.url}>{item.title}</a> 
+        </span> 
+        <span style={midColumn}>{item.author}</span> 
+        <span style={smallColumn}>{item.num_comments}</span> 
+        <span style={smallColumn}>{item.points}</span>
+        <span style={smallColumn}> 
+          <Button 
+            onClick={() => onDismiss(item.objectID)} className="button-inline"> 
+              Dismiss 
+          </Button> 
+        </span>  
+      </div>             
+      )
+    }
+  </div>    
+
+
+
+const Button = ({ onClick, className = '', children}) =>
+  <button 
+    onClick={onClick} 
+    className={className} 
+    type="button" > 
+      {children} 
+  </button> 
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+// Used to reload only the DOM and not the Browser
+if (module.hot) {
+    module.hot.accept();
+}
